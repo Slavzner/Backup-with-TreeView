84e0ab33a1e4712dbed4f252a96d68a42f32ba85
slavzner <slavapv2007@gmail.com>
2016-08-04 10:34:34 +0300

    Start

---
+++ GuiTest/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GuiTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceView = new System.Windows.Forms.TreeView();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sourceView
+            // 
+            this.sourceView.CheckBoxes = true;
+            this.sourceView.Location = new System.Drawing.Point(12, 12);
+            this.sourceView.Name = "sourceView";
+            this.sourceView.Size = new System.Drawing.Size(365, 446);
+            this.sourceView.TabIndex = 0;
+            this.sourceView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.sourceView_AfterSelect);
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(383, 12);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(75, 23);
+            this.btnSource.TabIndex = 1;
+            this.btnSource.Text = "Browse";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 480);
+            this.Controls.Add(this.btnSource);
+            this.Controls.Add(this.sourceView);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView sourceView;
+        private System.Windows.Forms.Button btnSource;
+    }
+}
+
---
+++ GuiTest/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GuiTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            var folderBrowser = new FolderBrowserDialog();
+            folderBrowser.ShowDialog();
+            ListDirectory(sourceView, folderBrowser.SelectedPath);
+        }
+
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private static TreeNode CreateDirectoryNode(DirectoryInfo directInfo)
+        {
+            var directoryNode = new TreeNode(directInfo.Name);
+            foreach (var directory in directInfo.GetDirectories())
+                directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            foreach (var file in directInfo.GetFiles())
+                directoryNode.Nodes.Add(new TreeNode(file.Name));
+            return directoryNode;
+        }
+
+        private void sourceView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            
+        }
+    }
+}
